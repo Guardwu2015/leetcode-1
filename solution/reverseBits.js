2e9dd4445355a68467bee0396f887ea0f70a5962
calabash519 <calabash519@gmail.com>
2017-07-30 13:52:49 +0800

    reverseBits add and commit

---
+++ solution/reverseBits.js
@@ -0,0 +1,10 @@
+var reverseBits = function (n) {
+    var str = n.toString(2);
+    var arr = str.split("");
+    arr.reverse();
+    var len = arr.length;
+    for (var i = 0; i < 32 - len; i++) {
+        arr.push("0");
+    }
+    return parseInt(arr.join(""), 2);
+};
